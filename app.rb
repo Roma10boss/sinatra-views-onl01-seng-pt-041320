963b6358ea4ee494864bd8c4c343e75a05d1c921
Romario Gustave <romagustave10@gmail.com>
2020-07-02 21:24:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		erb :index
-    end
- 
-    get "/info" do
-      erb :dogs
-    end
+		 "<h1>Hello World</h1>"
 	end
 end
